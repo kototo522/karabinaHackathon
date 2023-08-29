cb297726cb9a0c8a7546d1e830004463f8a45630
kototo522 <huyejiben@gmail.com>
2023-08-30 01:00:11 +0900

    add: カメラ機能の追加

--- app/src/main/java/com/example/karabina/ui/camera/CameraScreen.kt
+++ app/src/main/java/com/example/karabina/ui/camera/CameraScreen.kt
@@ -1,24 +1,39 @@
 package com.example.karabina.ui.camera
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-@OptIn(ExperimentalMaterial3Api::class)
+import androidx.core.app.ActivityCompat.startActivityForResult
+import coil.compose.AsyncImage
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun CameraScreen(){
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+    var bitmapImage by remember { mutableStateOf<Bitmap?>(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap: Bitmap? ->
+        bitmapImage = bitmap
+    }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
@@ -27,6 +42,11 @@ fun CameraScreen(){
             topBar = { TopAppBar(title = { Text(text = "カメラ") }) },
         ) { innerPadding ->
             Column(modifier = Modifier.padding(innerPadding)) {
+                if (cameraPermissionState.hasPermission) {
+                    launcher.launch(null)
+                } else {
+                    cameraPermissionState.launchPermissionRequest()
+                }
 
             }
         }
